5a917a56345649393e30cae181bbb1f979e150d2
ShushantVisions <shushant@visions.net.in>
2020-02-04 14:10:00 +0530

    reducer

--- resources/js/store/reducers.js
+++ resources/js/store/reducers.js
@@ -3,6 +3,6 @@ import { combineReducers } from 'redux'
 import auth from '../modules/auth/store/reduer'
 import user from '../modules/user/store/reducer'
 import articles from '../modules/article/store/reducer'
-import technologies from "../modules/technologies/store/reducer";
+import course_categories from "../modules/course_categories/store/reducer";
 import courses from "../modules/courses/store/reducer";
-export default combineReducers({ auth, user, articles, technologies,courses });
+export default combineReducers({ auth, user, articles, course_categories,courses });
