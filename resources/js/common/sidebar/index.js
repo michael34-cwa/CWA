962ae985a92f706043e04feb51741b70936b0d0d
MohitVisions <shushant@visions.net.in>
2020-02-13 14:48:48 +0530

    asdf

--- resources/js/common/sidebar/index.js
+++ resources/js/common/sidebar/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import {
-  NavbarToggler, Container, Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink 
+  NavbarToggler, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, Container, Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink 
 } from "reactstrap";
+
 import { NavLink as RRNavLink  } from "react-router-dom";
 const sideMenu = props => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -10,43 +11,122 @@ const sideMenu = props => {
 
   return (
     <div className="side-menu">
+    <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+       <NavLink tag={RRNavLink} to="/dashboard" activeClassName="active">  <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</NavLink>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+         <i class="fa fa-archive" aria-hidden="true"></i> Course Categories <span className="toggleicon"><i class="fa fa-plus" aria-hidden="true"></i> <i class="fa fa-minus" aria-hidden="true"></i></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        <Nav vertical  >
+        <NavItem>
+     <NavLink tag={RRNavLink} to="/course_categories" activeClassName="active">Course Categories List</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/course_categories/create" activeClassName="active">Add Course Category</NavLink> 
+        </NavItem>
+
+      </Nav>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+         <i class="fa fa-bar-chart" aria-hidden="true"></i> Courses <span className="toggleicon"><i class="fa fa-plus" aria-hidden="true"></i> <i class="fa fa-minus" aria-hidden="true"></i></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+           <Nav vertical>
+            <NavItem>
+     <NavLink tag={RRNavLink} to="/course_categories" activeClassName="active">Course List</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/courses/create" activeClassName="active">Add New Courses</NavLink> 
+        </NavItem>
+             
+      </Nav>
+      </div>
+     </div>
+</div>
+
+
+
+<div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapsefour">
+        <i class="fa fa-address-card" aria-hidden="true"></i>  Tasks <span className="toggleicon"><i class="fa fa-plus" aria-hidden="true"></i> <i class="fa fa-minus" aria-hidden="true"></i></span>
+        </a>
+      </h4>
+    </div>
+    <div id="collapsefour" class="panel-collapse collapse">
+      <div class="panel-body">
+           <Nav vertical>
+    
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/tasks" activeClassName="active">Tasks List</NavLink>
+        </NavItem>
+
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/tasks/create" activeClassName="active">Add New Task</NavLink> 
+        </NavItem>
+        
+      
+      </Nav>
+      </div>
+     </div>
+</div>
+
+
+
+</div>
+         
+
+ {/*
          <Nav vertical  >
     
   
-        {/*<Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
+      
+        <NavItem>
+
+          <NavLink tag={RRNavLink} to="/dashboard" activeClassName="active">  <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</NavLink>
+        </NavItem> 
+<Dropdown nav isOpen={dropdownOpen} toggle={toggle}>
           <DropdownToggle nav caret>
-            <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard
+           Course Categories
           </DropdownToggle>
            <DropdownMenu>
-            <DropdownItem header>
-              <i class="fa fa-adjust" aria-hidden="true"></i> Header
-            </DropdownItem>
-            <DropdownItem disabled>
-              <i class="fa fa-adjust" aria-hidden="true"></i> Action
-            </DropdownItem>
             <DropdownItem>
-              <i class="fa fa-adjust" aria-hidden="true"></i> Another Action
-            </DropdownItem>
-            <DropdownItem divider />
+            <NavLink tag={RRNavLink} to="/course_categories" activeClassName="active">Course Categories List</NavLink>
+                  </DropdownItem>
             <DropdownItem>
-              <i class="fa fa-adjust" aria-hidden="true"></i> Another Action
+              <NavLink tag={RRNavLink} to="/course_categories/create" activeClassName="active">Add Categories</NavLink> 
             </DropdownItem>
           </DropdownMenu> 
-        </Dropdown>*/}
-        <NavItem>
-          <NavLink tag={RRNavLink} to="/dashboard" activeClassName="active">Dashboard</NavLink>
-        </NavItem> 
-
-        <NavItem>
-            <NavLink tag={RRNavLink} to="/course_categories" activeClassName="active">Course Categories</NavLink>
-        </NavItem>
+        </Dropdown>
+        
         <NavItem>
           <NavLink tag={RRNavLink} to="/courses" activeClassName="active">Courses</NavLink> 
         </NavItem>
         <NavItem>
           <NavLink tag={RRNavLink} to="/tasks" activeClassName="active">Tasks</NavLink>
         </NavItem>
-          {/* <NavItem>
+          <NavItem>
           <NavLink href="#">
             <i class="fa fa-adjust" aria-hidden="true"></i> Another Link
           </NavLink>
@@ -75,8 +155,8 @@ const sideMenu = props => {
           <NavLink href="#">
             <i class="fa fa-adjust" aria-hidden="true"></i> Another Link
           </NavLink>
-        </NavItem> */}
-      </Nav>
+        </NavItem> 
+      </Nav>*/}
  
     </div>
   );
