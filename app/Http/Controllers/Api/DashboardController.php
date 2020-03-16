6e67d681c56544be3a38f1eba9cb3403ed51b4d3
mohitvisions <mohit@visions.net.in>
2020-03-16 17:21:30 +0530

    top

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -35,10 +35,8 @@ class DashboardController extends Controller
 
    
     private function thousandsCurrencyFormat($num)
-  {
-
-    if ($num > 1000) {
-
+  { 
+    if ($num > 1000) { 
       $x = round($num);
       $x_number_format = number_format($x);
       $x_array = explode(',', $x_number_format);
@@ -46,11 +44,9 @@ class DashboardController extends Controller
       $x_count_parts = count($x_array) - 1;
       $x_display = $x;
       $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
-      $x_display .= $x_parts[$x_count_parts - 1];
-
+      $x_display .= $x_parts[$x_count_parts - 1]; 
       return $x_display;
-    }
-
+    } 
     return $num;
   }
 }
